e034d93e20b38061ec816bfc0f601bf01202abfe
Marjana731 <marjanauk731@gmail.com>
2026-04-11 13:48:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ fetch(url)
         moviesConteiner.appendChild(card)
     });
 });
-const btn = document.querySelector('a')
+const btn = document.querySelector('.order-btn')
 btn.addEventListener('click', ()=>{
     gtag('event', 'click_start_btn');
     concole.log('Дані відправлено!');
